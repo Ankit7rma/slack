ee52ad8bb84095c63639e3d01b4d07a5b6339121
Ankit Sharma <ankitsharma290101@gmail.com>
2023-10-20 06:13:39 +0530

    Router Setup + Styled Component + Material Ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom"
+import Home from "./components/Home"
 
 
 
 function App() {
+  const appRouter = createBrowserRouter([
+    {
+      path:"/",
+      element:<Home/>,
+    }
+  ])
 
   return (
-    <>
+    <><RouterProvider router={appRouter}>
+
       <div className='text-3xl bg-red-300'>
        Lets Build Slack
       </div>
-      
+    </RouterProvider>
     </>
+      
   )
 }
 
---
+++ src/firebase.jsx
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAK5M-dICpgtTcgAZgIhPUKExw4rLFXzDo",
+    authDomain: "slack-clone-90196.firebaseapp.com",
+    projectId: "slack-clone-90196",
+    storageBucket: "slack-clone-90196.appspot.com",
+    messagingSenderId: "8544325914",
+    appId: "1:8544325914:web:73bb3ac942fb65ffbd1ca3"
+  };
