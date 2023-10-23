cf67ffffdabc0ba880db1306c3229639db339c93
Ankit Sharma <ankitsharma290101@gmail.com>
2023-10-23 21:32:13 +0530

    React-Spinkit

--- src/App.jsx
+++ src/App.jsx
@@ -8,23 +8,28 @@ import Chat from "./components/Chat"
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth } from "./firebase"
 import Login from "./components/Login"
-import { Spinner } from "react-spinkit"
+import  CubeGrid  from 'react-spinkit';
+
 
 
 
 function App() { 
-  const [ user , loading,error ] = useAuthState(auth)
-  if(loading){
+  const [ user , loading ] = useAuthState(auth)
+  if(true){return(
+    
     <AppLoading>
       <AppLoadingContents>
-        <img src="" alt=""/>
-        <Spinner
-        name="CubeGrid"
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRpDu4uROkHFyZTyrg1q1JFvMukUWX8PXPsmxygOvYT&s" alt="Slack-logo"/>
+        
+        <CubeGrid size={100}  
+        name="folding-cube"
         color="purple"
         fadeIn="none"
         />
+        
       </AppLoadingContents>
     </AppLoading>
+  )
   }
   
   
@@ -57,5 +62,22 @@ height: 100vh;
 
 `
 
-const AppLoading = styled.div``
-const AppLoadingContents = styled.div``
+const AppLoading = styled.div`
+display: grid;
+place-items: center;
+height: 100vh;
+width: 100%;
+`
+const AppLoadingContents = styled.div`
+text-align: center;
+padding-bottom:100px;
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+>img{
+  height: 200px;
+  padding: 20px;
+  margin-bottom:40px ;
+}
+`
--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+
+
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+  },
+  define: {
+    'process.env': {
+      REACT_SPINKIT_NO_STYLES: process.env.REACT_SPINKIT_NO_STYLES || false,
+    },
+  },
+
 })
